1c9480f523b7afdb9c364548231c98f82c72d03d
Jayaram-11 <jayaramg1104@gmail.com>
2025-08-29 21:39:25 +0530

    Updated the view account page

--- src/pages/MyAccount.jsx
+++ src/pages/MyAccount.jsx
@@ -4,7 +4,7 @@ const MyAccount = () => {
   return (
     <div style={{ textAlign: 'center', paddingTop: '4rem' }}>
       <h1 style={{ fontSize: '2rem', fontWeight: 'bold' }}>My Account</h1>
-      <p style={{ marginTop: '1rem' }}>This page will show your account details and purchased products.</p>
+      <p style={{ marginTop: '1rem' }}>This page will display detailed analytics for your products, including sales and earnings.</p>
     </div>
   );
 };
